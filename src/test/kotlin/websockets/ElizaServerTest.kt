5e06ce96b6a164ab242c302ad57176272231d773
Latrino <pablo_latre15@hotmail.com>
2025-11-03 19:07:08 +0100

    Implement onChat test in ElizaServerTest

--- src/test/kotlin/websockets/ElizaServerTest.kt
+++ src/test/kotlin/websockets/ElizaServerTest.kt
@@ -8,7 +8,7 @@ import jakarta.websocket.ContainerProvider
 import jakarta.websocket.OnMessage
 import jakarta.websocket.Session
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
@@ -36,7 +36,6 @@ class ElizaServerTest {
         assertEquals("The doctor is in.", list[0])
     }
 
-    @Disabled // Remove this line when you implement onChat
     @Test
     fun onChat() {
         logger.info { "Test thread" }
@@ -48,9 +47,28 @@ class ElizaServerTest {
         latch.await()
         val size = list.size
         // 1. EXPLAIN WHY size = list.size IS NECESSARY
+
+        /*  Guardamos el tamñao de la lista en la variable size porque el método await() puede
+         *  provocar que el hilo se bloquee hasta que el contador llegue a cero. Durante este tiempo,
+         *  el cliente puede recibir mensajes adicionales que se añaden a la lista, lo que haría que
+         *  el tamaño de la lista cambie después de la espera. Al almacenar el tamaño en una variable
+         *  separada, nos aseguramos de que estamos trabajando con un valor consistente y no afectado
+         *  por posibles cambios posteriores en la lista.
+         */
+
         // 2. REPLACE BY assertXXX expression that checks an interval; assertEquals must not be used;
+        assertTrue(size in 2..4)
+
         // 3. EXPLAIN WHY assertEquals CANNOT BE USED AND WHY WE SHOULD CHECK THE INTERVAL
+
+        /*  No se puede usar assertEquals porque el número exacto de mensajes recibidos puede variar
+         *  debido a que websockets es asíncrono y depende de la latencia de la red. Por eso, al verificar
+         *  un intervalo, nos aseguramos de que el test sea más robusto.
+         */
+
         // 4. COMPLETE assertEquals(XXX, list[XXX])
+        assertEquals("The doctor is in.", list[0])
+        assertTrue(list.any { it.contains("you") || it.contains("feel") || it.contains("?") })
     }
 }
 
@@ -84,6 +102,9 @@ class ComplexClient(
         // 5. COMPLETE if (expression) {
         // 6. COMPLETE   sentence
         // }
+        if (list.size == 3) {                                   // Si es el primer mensaje (bienvenida)
+            session.basicRemote.sendText("I am feeling sad")    // Enviamos mensaje de prueba
+        }
     }
 }
 
